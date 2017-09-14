6a93add677e60a0875930cee88fc544a0c67c6f9
DESKTOP-PN5IBQ4\Rebekah <rebekahlow.jy@u.nus.edu>
2017-09-14 22:37:43 +0800

    Add overlap key in experiment.json

--- js/ACPToolKit.js
+++ js/ACPToolKit.js
@@ -84,6 +84,7 @@ var ACPToolKit = (function () {
 
             $('.js-expt-technique').text(options.technique);
             $('.js-expt-granularity').text(options.granularity);
+            $('.js-expt-overlap').text(options.overlap);
             $('.js-expt-stimuli').text(options.stimuli);
 
             // Clean up DOM
